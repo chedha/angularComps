7f46a45f94bb375b4b8d33553065266397d97db2
chedha <chedha@gmail.com>
2024-11-28 13:08:39 -0500

    Add page routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ElementsHomeComponent } from './elements/elements-home/elements-home.component';
+import { CollectionsHomeComponent } from './collections/collections-home/collections-home.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'elements', component: ElementsHomeComponent },
+    { path: 'collections', component: CollectionsHomeComponent },
+    { path: '', component: HomeComponent },
+    { path: '**', component: NotFoundComponent }
+];
